9f16266fbc7a0e4bc6cd5e24fb65903b55777370
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-08-21 22:03:26 +0700

    Template Bagian Page added

---
+++ app/View/Components/Forms/Wizard.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\View\Components\Forms;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Wizard extends Component
+{
+    public $id;
+    public $label;
+    public $type;
+    public $placeholder;
+    public $options;
+    public $name;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($id, $label, $type = 'text', $placeholder, $options, $name)
+    {
+        $this->id = $id;
+        $this->label = $label;
+        $this->type = $type;
+        $this->placeholder = $placeholder;
+        $this->options = $options;
+        $this->name = $name;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.forms.wizard');
+    }
+}
