21c36b5f25f7d068705bdf186e54989a5b31bdd5
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-10-18 20:07:50 +0700

    Update: Create Page, Edit Page and All List on Table Page

---
+++ resources/views/control/admin/questions/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@push('subtitle')
+<p class="fs-2 w-75 p-0 my-auto sub-judul border border-white rounded-2 text-uppercase">
+  List Questions
+</p>
+@endpush
+
+@section('content')
+<div class="px-5">
+  <table class="table table-striped table-bordered" id="sortableTable">
+    <thead class="table-primary">
+      <tr>
+        <th width="5%">No</th>
+        <th>Pertanyaan</th>
+        <th>Type</th>
+        <th>Date Created</th>
+        <th width="15%">Action</th>
+      </tr>
+    </thead>
+    <tbody id="sortableBody">
+      @forelse($questions as $question)
+      <tr data-id="{{ $question }}">
+        <td class="handle cursor-grab">{{ $question->display_order }}</td>
+        <td>{{ $question->question_text }}</td>
+        <td>
+          @switch($question->package)
+          @case('awal_shift') Awal Shift @break
+          @case('saat_bekerja') Saat Bekerja @break
+          @case('setelah_istirahat') Setelah Istirahat @break
+          @case('akhir_shift') Akhir Shift @break
+          @case('leader') Leader @break
+          @default Tidak Teridenfikasi @break
+          @endswitch
+        </td>
+        <td>{{ $question->created_at }}</td>
+        <td>
+          <a href="{{ route('question.edit', $question->id) }}">Edit</a>
+          <form method="POST" action="{{ route('question.delete', $question->id) }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="return confirm('Apakah yakin dihapus?')">Delete</button>
+          </form>
+        </td>
+      </tr>
+      @empty
+      <tr>
+        <td colspan="5" class="text-center py-3 text-muted">Tidak ada data pertanyaan.</td>
+      </tr>
+      @endforelse
+    </tbody>
+  </table>
+
+  <div class="py-1 d-flex justify-content-between">
+    <a href="{{ route('question.create') }}" class="btn btn-primary text-white py-2 px-4 rounded-full">+</a>
+    <a href="#" class="btn btn-primary text-white py-2 px-4">Back</a>
+  </div>
+</div>
+@endsection
