c0ac89adc2c2517771027158a252aa631754e002
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-11-14 22:59:21 +0700

    update: ReportController

--- app/Http/Controllers/ControlLeader/ReportController.php
+++ app/Http/Controllers/ControlLeader/ReportController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
+    public function index()
+    {
+        return view("control.reports.index");
+    }
+
     public function monthly()
     {
         return view('control.reports.monthly');
